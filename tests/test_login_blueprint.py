761eaea9313c51e5150116adf87e23e066c8b7e8
jxltom <jxltom@gmail.com>
2017-01-10 14:49:14 +0800

    Add tests for configuration options and login_required in login blueprint

--- tests/test_login_blueprint.py
+++ tests/test_login_blueprint.py
@@ -9,6 +9,8 @@ class TestLoginBlueprint(unittest.TestCase):
     def setUp(self):
         config = Config()
         config.enable_login_view()
+        self.login_url = config.LOGIN_BLUEPRINT_PREFIX + config.LOGIN_VIEW_ROUTE
+        self.login_required_url = config.LOGIN_BLUEPRINT_PREFIX + '/login_required'
         self.app = create_app(config)
         self.app.config['LOGIN_USERNAME'] = 'admin'
         self.app.config['LOGIN_PASSWORD'] = 'admin'
@@ -16,18 +18,22 @@ class TestLoginBlueprint(unittest.TestCase):
     def test_login(self):
         """It is hard to test since the CSRF check."""
         app = self.app.test_client()
-        app.post(
-            '/login',
-            data=dict(username='admin', password='admin', remember=False, submit=True),
+        rv = app.post(
+            self.login_url,
+            data=dict(username='admin', password='admin', remember=False,
+                      submit=True),
             follow_redirects=False)
+        self.assertTrue(rv.status_code in (200, 302))
 
         app = self.app.test_client()
-        app.post(
-            '/login',
+        rv = app.post(
+            self.login_url,
             data=dict(username='admin', password='wrong'),
             follow_redirects=False)
+        self.assertTrue(rv.status_code in (200, 302))
 
     def test_login_required(self):
         app = self.app.test_client()
-        rv = app.get('/login_required')
-        self.assertTrue('Unauthorized' in rv.get_data(as_text=True))
+        rv = app.get(self.login_required_url)
+        self.assertTrue('Unauthorized' not in rv.get_data(as_text=True))
+        self.assertEqual(rv.status_code, 302)
