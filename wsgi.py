c5af7513edf17b743203acce7a68d0fc2584aa81
jxltom <jxltom@gmail.com>
2017-01-12 22:29:28 +0800

    Add heroku env support

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,13 @@
 from config import Config
-from flask_template import create_app
+from web_scheduler import create_app
 
-config = Config()
+
+config = Config(debug=False)
 config.enable_index_blueprint()
 config.enable_login_view()
 app = create_app(config)
 
 
 if __name__ == '__main__':
+    app.config['DBBUG'] = True
     app.run()
