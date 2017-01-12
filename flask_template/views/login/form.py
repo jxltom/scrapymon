d7e3c888d418305d8ea19323b801e642c8156456
jxltom <jxltom@gmail.com>
2017-01-12 22:41:31 +0800

    Fix additional super class in LoginForm

--- flask_template/views/login/form.py
+++ flask_template/views/login/form.py
@@ -3,7 +3,8 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Form
 from wtforms.validators import DataRequired
 
 
-class LoginForm(FlaskForm, Form):
+class LoginForm(FlaskForm):
+    """Login form class."""
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
