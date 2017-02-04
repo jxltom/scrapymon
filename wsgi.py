99792ab7ed49121f12b0cb0067508695bd09eaae
jxltom <jxltom@gmail.com>
2017-02-04 23:41:10 +0800

    Update worker import sequence

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from config import Config
 from flask_template import create_app
+from flask_template import worker
 
 app = create_app(Config(
     bootstrap=True,
@@ -9,7 +10,6 @@ app = create_app(Config(
     login=True,
     wechat=True,
 ))
-from flask_template import worker
 
 if __name__ == '__main__':
     app.config.update(DEBUG=True)
