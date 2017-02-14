1521bfd8d86cb415d34ebe990d95221829a9db0c
jxltom <jxltom@gmail.com>
2017-02-14 21:11:38 +0800

    Disable propagation of buttons in panel header

--- scrapymon/static/index/_base.js
+++ scrapymon/static/index/_base.js
@@ -1,8 +1,12 @@
+// Disable propagation of delete buttons in panel header so that they won't triggle toggle.
 $(document).ready(function () {
-    $('panel-title button').click(function (e) {
+    $('.panel-heading > .btn-group > button:nth-of-type(1)').click(function (e) {
         e.stopPropagation();
     });
-    $('panel-title button[data-toggle="dropdown"]').click(function () {
-        $('panel-title button[data-toggle="dropdown"]').dropdown('toggle')
+    $('.panel-heading > .btn-group > ul button').click(function (e) {
+        e.stopPropagation();
+    });
+    $('.panel-heading > .btn-group > button.disabled').click(function (e) {
+        e.stopPropagation();
     });
 });
