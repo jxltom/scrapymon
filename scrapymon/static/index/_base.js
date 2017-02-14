683c02b79e706dd1d4d1e90b1576cc14453c0d7a
jxltom <jxltom@gmail.com>
2017-02-14 21:36:43 +0800

    More control on delete button behaviour

--- scrapymon/static/index/_base.js
+++ scrapymon/static/index/_base.js
@@ -2,9 +2,11 @@
 $(document).ready(function () {
     $('.panel-heading > .btn-group > button:nth-of-type(1)').click(function (e) {
         e.stopPropagation();
+        $('.panel-heading > .btn-group > ul button').dropdown('toggle');
     });
     $('.panel-heading > .btn-group > ul button').click(function (e) {
         e.stopPropagation();
+        $('.panel-heading > .btn-group > ul button').dropdown('toggle');
     });
     $('.panel-heading > .btn-group > button.disabled').click(function (e) {
         e.stopPropagation();
