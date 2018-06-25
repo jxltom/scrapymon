7be6657d92729496efebcc80cfd75743f34fa60a
Unknown <jxltom@gmail.com>
2018-06-25 21:47:09 +0800

    Update js

--- scrapymon/static/scripts/app.js
+++ scrapymon/static/scripts/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     // Disable propagation of delete buttons in panel header so that they won't triggle toggle.
-    $('.panel-heading > .btn-group > button:nth-of-type(1)').click(function (e) {
+    $('.card-header > .btn-group > button:nth-of-type(1)').click(function (e) {
         e.stopPropagation();
-        $('.panel-heading > .btn-group > ul button').dropdown('toggle');
+        $('.card-header > .btn-group > ul button').dropdown('toggle');
     });
-    $('.panel-heading > .btn-group > ul button').click(function (e) {
+    $('.card-header > .btn-group > ul button').click(function (e) {
         e.stopPropagation();
-        $('.panel-heading > .btn-group > ul button').dropdown('toggle');
+        $('.card-header > .btn-group > ul button').dropdown('toggle');
     });
-    $('.panel-heading > .btn-group > button.disabled').click(function (e) {
+    $('.card-header > .btn-group > button.disabled').click(function (e) {
         e.stopPropagation();
     });
 
